6f8a8770a0298d3a765d50a9279eb01af4162ce6
Barock <aabell3@wgu.edu>
2017-04-08 23:56:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,24 @@
+#!/bin/sh
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0'`;
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+
+# go to root
 cd
+# disable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+
+# install wget and curl
+apt-get update;apt-get -y install wget curl;
+
+cd
+apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
+cd
+
 wget http://www.webmin.com/jcameron-key.asc
 apt-key add jcameron-key.asc
 echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
@@ -9,3 +29,6 @@ apt-get -y install webmin
 sed -i "s/ssl=1/ssl=0/g" /etc/webmin/miniserv.conf
 /etc/init.d/webmin restart
 cd
+apt-get install apache2
+apt-get install php5 php-pear php5-mysql php5-curl php5-fpm libapache2-mod-php5
+service apache2 restart
